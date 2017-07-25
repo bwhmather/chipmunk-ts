a46f6afead8d98f9a6554c4472dd1d452cf77343
Ben Mather <bwhmather@bwhmather.com>
2017-07-25 09:29:59 +0100

    Fix pylint errors in vect

--- src/vect.ts
+++ src/vect.ts
@@ -82,15 +82,16 @@ export function vdot2(x1: number, y1: number, x2: number, y2: number): number {
 }
 
 /// Returns the length of v.
-export function vlength(v: Vect): number {
-    return Math.sqrt(vdot(v, v));
+export function vlength(vect: Vect): number {
+    return Math.sqrt(vdot(vect, vect));
 }
 
 export function vlength2(x: number, y: number): number {
     return Math.sqrt(x * x + y * y);
 }
 
-/// Check if two vectors are equal. (Be careful when comparing floating point numbers!)
+/// Check if two vectors are equal. (Be careful when comparing floating point
+/// numbers!)
 export function veql(v1: Vect, v2: Vect): boolean {
     return (v1.x === v2.x && v1.y === v2.y);
 }
@@ -106,34 +107,36 @@ export function vsub(v1: Vect, v2: Vect): Vect {
 }
 
 /// Negate a vector.
-export function vneg(v: Vect): Vect {
-    return new Vect(-v.x, -v.y);
+export function vneg(vect: Vect): Vect {
+    return new Vect(-vect.x, -vect.y);
 }
 
 /// Scalar multiplication.
-export function vmult(v: Vect, s: number): Vect {
-    return new Vect(v.x * s, v.y * s);
+export function vmult(vect: Vect, s: number): Vect {
+    return new Vect(vect.x * s, vect.y * s);
 }
 
 /// 2D vector cross product analog.
-/// The cross product of 2D vectors results in a 3D vector with only a z component.
-/// This function returns the magnitude of the z value.
+/// The cross product of 2D vectors results in a 3D vector with only a z
+/// component.  This function returns the magnitude of the z value..
 export function vcross(v1: Vect, v2: Vect): number {
     return v1.x * v2.y - v1.y * v2.x;
 }
 
-export function vcross2(x1: number, y1: number, x2: number, y2: number): number {
+export function vcross2(
+    x1: number, y1: number, x2: number, y2: number,
+): number {
     return x1 * y2 - y1 * x2;
 }
 
 /// Returns a perpendicular vector. (90 degree rotation)
-export function vperp(v: Vect): Vect {
-    return new Vect(-v.y, v.x);
+export function vperp(vect: Vect): Vect {
+    return new Vect(-vect.y, vect.x);
 }
 
 /// Returns a perpendicular vector. (-90 degree rotation)
-export function vpvrperp(v: Vect): Vect {
-    return new Vect(v.y, -v.x);
+export function vpvrperp(vect: Vect): Vect {
+    return new Vect(vect.y, -vect.x);
 }
 
 /// Returns the vector projection of v1 onto v2.
@@ -141,7 +144,8 @@ export function vproject(v1: Vect, v2: Vect): Vect {
     return vmult(v2, vdot(v1, v2) / vlengthsq(v2));
 }
 
-/// Uses complex number multiplication to rotate v1 by v2. Scaling will occur if v1 is not a unit vector.
+/// Uses complex number multiplication to rotate v1 by v2. Scaling will occur
+/// if v1 is not a unit vector.
 export function vrotate(v1: Vect, v2: Vect): Vect {
     return new Vect(v1.x * v2.x - v1.y * v2.y, v1.x * v2.y + v1.y * v2.x);
 }
@@ -151,9 +155,10 @@ export function vunrotate(v1: Vect, v2: Vect): Vect {
     return new Vect(v1.x * v2.x + v1.y * v2.y, v1.y * v2.x - v1.x * v2.y);
 }
 
-/// Returns the squared length of v. Faster than vlength() when you only need to compare lengths.
-export function vlengthsq(v: Vect): number {
-    return vdot(v, v);
+/// Returns the squared length of v. Faster than vlength() when you only need to
+/// compare lengths.
+export function vlengthsq(vect: Vect): number {
+    return vdot(vect, vect);
 }
 
 export function vlengthsq2(x: number, y: number): number {
@@ -166,18 +171,19 @@ export function vlerp(v1: Vect, v2: Vect, t: number): Vect {
 }
 
 /// Returns a normalized copy of v.
-export function vnormalize(v: Vect): Vect {
-    return vmult(v, 1 / vlength(v));
+export function vnormalize(vect: Vect): Vect {
+    return vmult(vect, 1 / vlength(vect));
 }
 
-/// Returns a normalized copy of v or vzero if v was already vzero. Protects against divide by zero errors.
-export function vnormalize_safe(v: Vect): Vect {
-    return (v.x === 0 && v.y === 0 ? vzero : vnormalize(v));
+/// Returns a normalized copy of v or vzero if v was already vzero. Protects
+/// against divide by zero errors.
+export function vnormalize_safe(vect: Vect): Vect {
+    return (vect.x === 0 && vect.y === 0 ? vzero : vnormalize(vect));
 }
 
 /// Clamp v to length len.
-export function vclamp(v: Vect, len: number): Vect {
-    return (vdot(v, v) > len * len) ? vmult(vnormalize(v), len) : v;
+export function vclamp(vect: Vect, len: number): Vect {
+    return (vdot(vect, vect) > len * len) ? vmult(vnormalize(vect), len) : vect;
 }
 
 /// Linearly interpolate between v1 towards v2 by distance d.
@@ -190,7 +196,8 @@ export function vdist(v1: Vect, v2: Vect): number {
     return vlength(vsub(v1, v2));
 }
 
-/// Returns the squared distance between v1 and v2. Faster than vdist() when you only need to compare distances.
+/// Returns the squared distance between v1 and v2. Faster than vdist() when you
+/// only need to compare distances.
 export function vdistsq(v1: Vect, v2: Vect): number {
     return vlengthsq(vsub(v1, v2));
 }
@@ -206,13 +213,17 @@ export function vslerp(v1: Vect, v2: Vect, t: number): Vect {
 
     if (omega) {
         const denom = 1 / Math.sin(omega);
-        return vadd(vmult(v1, Math.sin((1 - t) * omega) * denom), vmult(v2, Math.sin(t * omega) * denom));
+        return vadd(
+            vmult(v1, Math.sin((1 - t) * omega) * denom),
+            vmult(v2, Math.sin(t * omega) * denom),
+        );
     } else {
         return v1;
     }
 }
 
-/// Spherical linearly interpolate between v1 towards v2 by no more than angle a radians
+/// Spherical linearly interpolate between v1 towards v2 by no more than angle
+/// `a` radians.
 export function vslerpconst(v1: Vect, v2: Vect, a: number): Vect {
     const angle = Math.acos(vdot(v1, v2));
     return vslerp(v1, v2, Math.min(a, angle) / angle);
@@ -224,11 +235,12 @@ export function vforangle(a: number): Vect {
 }
 
 /// Returns the angular direction v is pointing in (in radians).
-export function vtoangle(v: Vect): number {
-    return Math.atan2(v.y, v.x);
+export function vtoangle(vect: Vect): number {
+    return Math.atan2(vect.y, vect.x);
 }
 
-///	Returns a string representation of v. Intended mostly for debugging purposes and not production use.
-export function vstr(v: Vect): string {
-    return "(" + v.x.toFixed(3) + ", " + v.y.toFixed(3) + ")";
+/// Returns a string representation of v. Intended mostly for debugging purposes
+/// and not production use.
+export function vstr(vect: Vect): string {
+    return "(" + vect.x.toFixed(3) + ", " + vect.y.toFixed(3) + ")";
 }
